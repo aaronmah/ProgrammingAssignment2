ba58e2eb776f5c9c2cedc50eb9ef3f9d92d8c56a
mahfasa <aa.mahh@gmail.com>
2020-01-28 18:13:37 -0800

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,25 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        #instantiate the matrix to store
+        cache <- matrix()
+        
+        #solve it
+        solveMatrix <- function() {
+                solve(x) 
+                x
+        }
+        
+        #get cache
+        getCache <- function() cache
+        
+        #setCache
+        setCache <- function(solved) {
+                cache <<- solved 
+                cache
+                } 
+
+        cache
 }
 
 
@@ -12,4 +31,45 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        #see if there's a value in cache
+        #if there is, return it
+        #if there isn't, call solve function and set it
+        #store in cache
+        #return solve
+        
+        cache <- x$getCache()
+        
+        if(!is.na(cache)) return(cache)
+        
+        solved <- x$solveMatrix()
+        
+        x$setCache(solved)
+        
+        solved
+}
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
